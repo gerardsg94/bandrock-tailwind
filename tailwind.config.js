3dbd3f84dc42bec5ef97c1894fd9307f07eaaeac
gerardsg94 <gerard_sanchez94@uoc.edu>
2022-12-29 14:25:33 +0100

    nav changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,28 +20,14 @@ module.exports = {
         '65':'65%',
         '3.75':'15px',
         '12.5':'50px',
-        '56.25':'56.25%',
-        '18':'75px'
-      },
-      backgroundImage: {
-        'header-blog':'url(../images/concert-2.jpg)'
-      },
-      backgroundPosition: {
-        'p-50':'50px'
+        '56.25':'56.25%'
       },
       fontSize: {
         'xl.1':'1.2rem'
       },
-      gridTemplateRows: {
-        '1-2(350)':'1fr 350px 350px'
-      },
       screens: {
         'screen-w-672-h-432': {'raw': '(min-width: 672px) and (min-height: 432px)'},
-        '1400': {'raw': '(min-width:1400px)'},
-        '1200':{'raw': '(min-width:1200px)'},
-        '992': {'raw': '(min-width:992px)'},
-        '768': {'raw': '(min-width:768px)'},
-        '576': {'raw': '(min-width:576px)'},
+        '1400': {'raw': '(min-width:1400px)'}
       }
     },
   },
